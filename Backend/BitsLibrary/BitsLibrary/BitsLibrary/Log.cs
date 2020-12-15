dc3f4ffc0b570d3bf863fab1381be9440ebf7da5
Gabriel Wrede <gawr0001@student.hv.se>
2020-12-16 00:15:59 +0100

    Debug logging
    För att se logs:
    https://155.4.70.67/API/Logging

---
+++ Backend/BitsLibrary/BitsLibrary/BitsLibrary/Log.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Threading;
+using System.Text.Json;
+
+namespace BitsLibrary
+{
+    public static class Log
+    {
+        private const string APIS_SERVER = "https://192.168.1.149/API/";
+        private const string API = "Logging/api/csharp/set";
+
+        public static Answer Add(Logs.CTypeErrorLog log) 
+        {
+            Answer answer = new Answer();
+
+            answer = RestCall(log);
+
+            return answer;
+        }
+
+        private static Answer RestCall(Logs.CTypeErrorLog log) => RestCallAsync(log).GetAwaiter().GetResult();
+        private static async Task<Answer> RestCallAsync(Logs.CTypeErrorLog log)
+        {
+            try
+            {
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+                HttpClient client = new HttpClient(clientHandler);
+
+                client.BaseAddress = new Uri(APIS_SERVER + API);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string text = log.ToJSONString();
+
+                HttpContent content = new StringContent(log.ToJSONString(), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                return JsonToAnswer(responseString);
+            }
+            catch (Exception ex)
+            {
+                return Answer.Error("[Log: RestCallAsync()] " + ex.ToString());
+            }
+        }
+        private static Answer JsonToAnswer(string json)
+        {
+            //"isSuccess":true
+            //"json":null
+            json = json.Trim(new Char[] { '{', '}' });
+            string[] values = json.Split(',');
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = values[i].Substring(values[i].IndexOf(":") + 1);
+            }
+
+            Answer answer = new Answer();
+
+            answer.IsASuccess = bool.Parse(values[0]);
+            answer.Json = values[1];
+
+            if (values[1] == "null")
+            {
+                answer.Json = null;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Backend/BitsLibrary/BitsLibrary/BitsLibrary/Logs/CTypeErrorLog.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitsLibrary.Logs
+{
+    public class CTypeErrorLog
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        public string Source { get; set; }
+        public string HelpLink { get; set; }
+        public int HResult { get; set; }
+        public string TargetSiteName { get; set; }
+        public string HowIsThis { get; set; }
+        public DateTime Datum { get; set; }
+
+        public string ToJSONString()
+        {
+            string json = "{";
+
+            json += "\"Id\":" + this.Id + ",";
+            json += "\"Message\":\"" + this.Message + "\",";
+            json += "\"StackTrace\":\"" + this.StackTrace + "\",";
+            json += "\"Source\":\"" + this.Source + "\",";
+            json += "\"HelpLink\":\"" + this.HelpLink + "\",";
+            json += "\"HResult\":" + this.HResult + ",";
+            json += "\"TargetSiteName\":\"" + this.TargetSiteName + "\",";
+            json += "\"HowIsThis\":\"" + this.HowIsThis + "\",";
+            json += "\"Datum\":\"" + GetLocalTime() + "\"";
+
+            json += "}";
+
+            return json;
+        }
+
+        private string GetLocalTime()
+        {
+            return "" + StringTime(4, this.Datum.Year) + "-" + StringTime(2, this.Datum.Month) + 
+                "-" + StringTime(2, this.Datum.Day) + "T" + StringTime(2, this.Datum.Hour) + 
+                ":" + StringTime(2, this.Datum.Minute) + ":" + StringTime(2, this.Datum.Second);
+        }
+        private static string StringTime(int length, int input)
+        {
+            if (length < input.ToString().ToCharArray().Length)
+                return string.Empty;
+
+            if (length == input.ToString().ToCharArray().Length)
+                return input.ToString();
+
+            string text = "";
+            int timeLength = input.ToString().ToCharArray().Length;
+
+            while (length != timeLength)
+            {
+                text += "0";
+                timeLength++;
+            }
+            text += input.ToString();
+
+            return text;
+        }
+
+        public Dictionary<string, string> ToDictionary()
+        {
+            var value = new Dictionary<string, string>
+            {
+                {"Id", this.Id.ToString()},
+                {"Message",this.Message},
+                {"StackTrace",this.StackTrace},
+                {"Source",this.Source},
+                {"HelpLink",this.HelpLink},
+                {"HResult",this.HResult.ToString()},
+                {"TargetSiteName",this.TargetSiteName},
+                {"HowIsThis",this.HowIsThis},
+                {"Datum",GetLocalTime()}
+            };
+            return value;
+        }
+        public static CTypeErrorLog ExceptionToCTypeErrorLog(Exception exception, string howIsThis)
+        {
+            CTypeErrorLog errorLog = new CTypeErrorLog();
+
+            errorLog.Message = exception.Message;
+            errorLog.StackTrace = exception.StackTrace;
+            errorLog.Source = exception.Source;
+            errorLog.HelpLink = exception.HelpLink;
+            errorLog.HResult = exception.HResult;
+            errorLog.TargetSiteName = exception.TargetSite.Name;
+            errorLog.HowIsThis = howIsThis;
+            errorLog.Datum = DateTime.Now;
+
+            return errorLog;
+        }
+    }
+}
