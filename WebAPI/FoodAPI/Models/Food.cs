6fef5ec448202c4472d8edb006c8dfb2f29d0fc0
Adamjohansson123 <Adamjohansson123@users.noreply.github.com>
2020-12-18 11:16:44 +0100

    Lagt till modeller

---
+++ WebAPI/FoodAPI/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodAPI.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public Restaurant Id_Restaurant { get; set; }
+    }
+}
---
+++ WebAPI/FoodAPI/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerAdress { get; set; }
+        public string CustomerZIP { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public City Id_City { get; set; }
+    }
+}
